f1eb1b93d96511eac7d50810e24ed64e46318eb3
Ramish <agamatimtim@gmail.com>
2021-05-07 22:06:55 +0300

    autocomplete_implemented

--- autocompleteApi.php
+++ autocompleteApi.php
@@ -9,11 +9,11 @@ $conn=mysqli_connect($host,$username,$password,$databasename);
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	$search = $_POST['search'];
-	$q = "SELECT page_title FROM pages where page_title like '%".$search."%' LIMIT 5";
+	$q = "SELECT page_title FROM pages where page_title like '%".$search."%' LIMIT 10";
 	$result =mysqli_query($conn,$q);
 	if (mysqli_num_rows($result)) {
 	     while($row=mysqli_fetch_assoc($result)){
-		 echo '<a href ="#" class = "list-group list-group-item-action border p-2"">'
+		 echo '<a href ="#" class = "list-group list-group-item-action border p-2  hover-item" >'
 					.$row['page_title'].'<a/> ';
 		 }
 	}
